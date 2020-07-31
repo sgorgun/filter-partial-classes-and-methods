670982cb8e5e2c4d56aa9ea2fe15bb173b3e429e
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 09:06:50 +0000

    Upload New File

---
+++ FilerByDigit/ArrayExtensionClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilterByDigit
+{
+    /// <summary>
+    /// Class that contains filter operations of arrays.
+    /// </summary>
+    public static partial class ArrayExtension
+    {
+        /// <summary>
+        /// Presents digit.
+        /// </summary>
+        public static int Digit
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        
+        static partial void AddAccordingToPredicate(ICollection<int> collection, int item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
