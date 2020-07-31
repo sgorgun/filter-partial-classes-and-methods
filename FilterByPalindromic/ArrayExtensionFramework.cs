307a0d62483b2de715bae08a47f23b66128af8e5
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 09:08:18 +0000

    Upload New File

---
+++ FilterByPalindromic/ArrayExtensionFramework.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace FilterByPalindromic
+{
+    public static partial class ArrayExtension
+    {
+        /// <summary>
+        /// Returns new array of elements that satisfy some predicate.
+        /// </summary>
+        /// <param name="source">Source array.</param>
+        /// <returns>New array of elements that satisfy some predicate.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
+        public static int[] FilterByPredicate(this int[] source)
+        {
+            throw new NotImplementedException();
+        }
+
+        static partial void AddAccordingToPredicate(ICollection<int> list, int item);
+    }
+}
