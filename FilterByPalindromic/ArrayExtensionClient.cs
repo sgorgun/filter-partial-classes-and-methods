208baf3720d3a29047a115779cafe57669678353
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 10:54:15 +0000

    Update ArrayExtensionClient.cs

--- FilterByPalindromic/ArrayExtensionClient.cs
+++ FilterByPalindromic/ArrayExtensionClient.cs
@@ -5,7 +5,7 @@ namespace FilterByPalindromic
 {
     public static partial class ArrayExtension
     {
-        static partial void AddAccordingToPredicate(ICollection<int> list, int item)
+        static partial void AddAccordingToPredicate(ICollection<int> collection, int item)
         {
             throw new NotImplementedException();
         }
