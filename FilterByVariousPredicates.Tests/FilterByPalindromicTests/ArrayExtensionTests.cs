a17167ae8c58ffe130a661a408c850ba218188d5
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 12:29:30 +0000

    Update ArrayExtensionTests.cs

--- FilterByVariousPredicates.Tests/FilterByPalindromicTests/ArrayExtensionTests.cs
+++ FilterByVariousPredicates.Tests/FilterByPalindromicTests/ArrayExtensionTests.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
-using FilterByDigit;
+using FilterByPalindromic;
 using NUnit.Framework;
 
 #pragma warning disable CA1707
@@ -15,7 +15,7 @@ namespace FilterByVariousPredicates.Tests.FilterByPalindromicTests
         [TestCase(new[] { 17, 82, 45, 58, 881, 117, 25 }, ExpectedResult = new int[] { })]
         [TestCase(new[] { 2212332, 0, 1405644, 12345, 1, -1236674, 123321, 1111111 }, ExpectedResult = new[] { 0, 1, 123321, 1111111 })]
         [TestCase(new[] { 1111111112, 987654, -24, 1234654321, 32, 1005 }, ExpectedResult = new int[] { })]
-        [TestCase(new[] { -27, 987656789, 7557, int.MaxValue, 7556, 7243, 7243427, int.MinValue }, ExpectedResult = new[] { 7557, 7243427 })]
+        [TestCase(new[] { -27, 987656789, 7557, int.MaxValue, 7556, 7243, 7243427, int.MinValue }, ExpectedResult = new[] { 987656789, 7557, 7243427 })]
         [TestCase(new[] { 111, 111, 111, 11111111 }, ExpectedResult = new[] { 111, 111, 111, 11111111 })]
         [TestCase(new[] { -1, 0, 111, -11, -1 }, ExpectedResult = new[] { 0, 111 })]
         [TestCase(new[] { 0, 1, 2, 3, 4 }, ExpectedResult = new[] { 0, 1, 2, 3, 4 })]
