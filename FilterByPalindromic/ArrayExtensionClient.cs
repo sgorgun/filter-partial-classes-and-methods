cee66a17adf7ac11d8622b06a911a73ac670fc2a
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 09:08:06 +0000

    Upload New File

---
+++ FilterByPalindromic/ArrayExtensionClient.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace FilterByPalindromic
+{
+    public static partial class ArrayExtension
+    {
+        static partial void AddAccordingToPredicate(ICollection<int> list, int item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
